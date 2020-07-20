70ce96c4a37667dd42be2be7e99606352f527251
Mazhar Ali Baig <2018145@iiitdmj.ac.in>
2020-07-20 18:51:04 +0530

    Header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Header from './Components/Header';
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
