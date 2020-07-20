27d417b4ad616c87a1c005ae0f13abc48f978285
abhisheknaiidu <geekyabhi12@gmail.com>
2020-07-20 20:36:03 +0530

    Added Google Login Button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,6 +14,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Header from '../Components/Header';
 import SEO from '../Components/Seo';
+import GoogleIcon from '../assets/google.png'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -103,7 +104,19 @@ export default function Login() {
       </div>
       <Box mt={8}>
       </Box>
+      <LoginWithGoogle/>
     </Container>
    </>
   );
+}
+
+function LoginWithGoogle() {
+    return (
+        <Button fullWidth>
+            <img src={GoogleIcon}
+            style={{width: '25px', height: '25px'}}
+            alt="Google Icon" />
+            Log In with Google
+        </Button>
+    )
 }
