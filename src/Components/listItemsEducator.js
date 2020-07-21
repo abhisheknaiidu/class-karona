f3d9d7601c1c8d6597c1a02f553cecf74ff0dc55
abhisheknaiidu <geekyabhi12@gmail.com>
2020-07-21 17:12:13 +0530

    Removed Unnecessary elements

--- src/Components/listItemsEducator.js
+++ src/Components/listItemsEducator.js
@@ -1,23 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import List from '@material-ui/core/List';
-import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import PeopleIcon from '@material-ui/icons/People';
-import BarChartIcon from '@material-ui/icons/BarChart';
 import AssignmentIcon from '@material-ui/icons/Assignment';
-import ClassIcon from '@material-ui/icons/Class';
-
 
 export const secondaryListItems = (
   <div>
@@ -29,28 +14,3 @@ export const secondaryListItems = (
       </ListItem>
   </div>
 );
-
-{/* <ListItem button>
-<ListItemIcon>
-  <ClassIcon />
-</ListItemIcon>
-<ListItemText primary="My Lectures" />
-</ListItem> */}
-
-{/* <ListItem button onClick={handleClick}>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Inbox" />
-          {open ? <ExpandLess /> : <ExpandMore />}
-        </ListItem>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItem button className={classes.nested}>
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
-              <ListItemText primary="Starred" />
-            </ListItem>
-          </List>
-        </Collapse> */}
